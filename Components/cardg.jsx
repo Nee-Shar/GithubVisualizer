721964c08e44099c993a65a29b123f883b39f8ee
Neeraj Sharma <neeraj716sharma@gmail.com>
2023-06-23 13:13:25 +0530

    .

--- Components/cardg.jsx
+++ Components/cardg.jsx
@@ -41,8 +41,7 @@ function Cardg({
     //     display: "flex",
     //     justifyContent: "center",
     //     alignItems: "center",
-    //     height: "100vh",
-    //   }}
+
     // >
     <div
       style={{
