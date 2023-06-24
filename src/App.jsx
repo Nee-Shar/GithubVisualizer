9b5be46313e7f035c05a75c4875143e8fe7dc736
Neeraj Sharma <neeraj716sharma@gmail.com>
2023-06-24 12:38:50 +0530

    Improved Animation

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,19 @@ import ResponsiveAppBar from "../Components/navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import BouncingImage from "../Components/bounce.jsx";
 import Lander from "../Components/lander";
-
+import Lottie from "react-lottie";
+import animationData from "../Lotties/github";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   const [count, setCount] = useState(0);
-
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
   return (
     <Router>
       <Routes>
@@ -20,7 +28,10 @@ function App() {
           element={
             <div>
               <Main />
-              <BouncingImage />
+              <div style={{ marginTop: "-4%" }}>
+                <Lottie options={defaultOptions} height={200} width={200} />
+              </div>
+              {/* <BouncingImage /> */}
               <Lander />
             </div>
           }
